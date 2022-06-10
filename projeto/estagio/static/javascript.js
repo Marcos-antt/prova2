e69c4d5df2c0788a2df545ae5e9d8fb51ab6fde0
Marcos-TI <marksilva147@hotmail.com>
2022-06-10 14:37:04 -0300

    Ajax form -> Render pagina inicial

--- projeto/estagio/static/javascript.js
+++ projeto/estagio/static/javascript.js
@@ -1,30 +1,31 @@
-(function(win,doc){
-    (function(win,doc){
+(function(win, doc) {
+    (function(win, doc) {
         'use strict';
         //Ajax do Formulário
-        if(doc.querySelector('#form')){
-            let form=doc.querySelector('#form');
-            function sendForm(event)
-            {
+        if (doc.querySelector('#form')) {
+            let form = doc.querySelector('#form');
+
+            function sendForm(event) {
                 event.preventDefault();
                 let data = new FormData(form);
                 let ajax = new XMLHttpRequest();
                 let token = doc.querySelectorAll('input')[0].value;
                 ajax.open('POST', form.action);
-                ajax.setRequestHeader('X-CSRF-TOKEN',token);
-                ajax.onreadystatechange = function()
-                {
-                    if(ajax.status === 200 && ajax.readyState === 4){
+                ajax.setRequestHeader('X-CSRF-TOKEN', token);
+                ajax.onreadystatechange = function() {
+                    if (ajax.status === 200 && ajax.readyState === 4) {
                         let result = doc.querySelector('#result');
                         result.innerHTML = 'Postagem realizada!';
                         result.classList.add('alert');
                         result.classList.add('alert-success');
+                        setTimeout("location.href = '/';", 2000);
+
+
                     }
                 }
                 ajax.send(data);
-                form.reset();
             }
-            form.addEventListener('submit',sendForm,false);
+            form.addEventListener('submit', sendForm, false);
         }
-    })(window,document);
-})(window,document)
+    })(window, document);
+})(window, document)
